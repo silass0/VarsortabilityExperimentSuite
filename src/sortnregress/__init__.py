cd029e3e55e06749cd14c61e2c070f288cc40952
tossefinger <silasnostvik@hotmail.com>
2022-12-05 11:08:59 +0100

    Added polynomial sortnregress

--- src/sortnregress/__init__.py
+++ src/sortnregress/__init__.py
@@ -1,4 +1,8 @@
 import numpy as np
+import itertools
+import re
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import (LinearRegression,
                                   LassoLarsIC,
                                   LassoLarsCV)
@@ -53,3 +57,83 @@ def sortnregress(X, regularisation='1-p', random_order=False):
             raise ValueError("no such regularization")
 
     return W
+
+def sortnregress_poly(X, degree=2, max_indegree=3, include_interactions=True, random_order=False):
+    """Takex n x d data, assumes order is given by increased variance,
+    and regresses each node polynomially onto those with lower variance, using
+    edge coefficients as structure estimates. Features are eliminated #TODO
+
+    Args:
+        X (ndarray): data array
+        degree (int, optional): Degree of polynomial regression
+        max_indegree (int, optional): Maximal indegree of every node in network. Defaults to 3.
+        include_interactions (bool, optional): Include covariate interactions in regression step.
+        random_order (bool, optional): If True, performs randomregress instead. Defaults to False.
+    """
+
+    d = X.shape[1]
+    W = np.zeros((d, d))
+    #W1 = np.zeros((d, d)) #linear regression coefficients, (cov, target)
+    #W2 = np.zeros((d, d, d)) #quadratic/interaction regression coefficients, (target, (cov1, cov2))
+    increasing = np.argsort(np.var(X, 0))
+
+    if random_order:
+        np.random.shuffle(increasing)
+
+    for k in range(1, d):
+        covariates = increasing[:k]
+        target = increasing[k]
+
+        # Feature selection
+        best_model = {"model":"model", "subset":"array", "score":-np.inf} # model, subset, score
+        if len(covariates) <= max_indegree:
+            subsets = [covariates]
+            W1 = np.zeros((len(covariates))) #order analogue to covariates
+            W2 = np.zeros((len(covariates), len(covariates)))
+        else: #will not check lesser subsets, as they will result in lower score
+            subsets = list(itertools.combinations(covariates, max_indegree))
+            W1 = np.zeros((len(max_indegree)))
+            W2 = np.zeros((len(max_indegree), len(max_indegree)))
+        for subset in subsets:
+            model = make_pipeline(PolynomialFeatures(degree=degree), LinearRegression())
+            model.fit(X[:, subset], X[:, target].ravel())
+            score = model.score(X[:, subset], X[:, target].ravel())
+            if score > best_model["score"]:
+                best_model["model"] = model
+                best_model["subset"] = subset
+                best_model["score"] = score 
+        model = best_model["model"]
+        subset = best_model["subset"]
+
+        #build W1 and W2
+        # feature order: x0, x1, x2, x0^2, x0 x1, x0 x2, x1^2, x1 x2, x2^2 (for 2 covariates)
+        for i, feat_name in enumerate(model['polynomialfeatures'].get_feature_names()):
+            pair_search = re.search(r"x([0-9]+) x([0-9]+)", feat_name)
+            if pair_search: #interactions
+                match = [int(gr) for gr in pair_search.groups()]
+                W2[match[0], match[1]] = W2[match[1], match[0]] =  model['linearregression'].coef_[i]
+                continue
+            quad_search = re.search(r"x([0-9]+)\^2", feat_name)
+            if quad_search: #quadratics
+                match = int(quad_search.groups()[0])
+                W2[match, match] = model['linearregression'].coef_[i]
+                continue
+            lin_search = re.search(r"x([0-9]+)", feat_name)
+            if lin_search:
+                match = int(lin_search.groups()[0])
+                W1[match] = model['linearregression'].coef_[i]
+                continue
+
+        #aggregate polynomial weights and interactions into a single output matrix W 
+        heuristic = {"lin":0.7, "quad":0.2, "inters":0.1} #for single cov, target
+        for i, cov in enumerate(best_model["subset"]):
+            w = 0
+            w += np.abs(W1[i]) * heuristic["lin"]
+            w += np.abs(W2[i, i]) * heuristic["quad"]
+            inters = [w for j, w in enumerate(W2[i, :]) if j!=i]
+            w += np.abs(np.mean(inters)) * heuristic["inters"]
+            W[cov, target] = w
+        #Possible TODO: make it so interactions only exists for dependent covariates (but regression should assign interactions with small weights if indep)
+        #Possible TODO: Develop better heuristic for weigh aggregation (importance for various weight degrees)
+    return W
+
