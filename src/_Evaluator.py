98e97e2a957f4e635ec7a7383ec88b2606563b44
Silas' Desktop <silasnostvik@hotmail.com>
2023-01-11 20:30:28 +0100

    remove SID

--- src/_Evaluator.py
+++ src/_Evaluator.py
@@ -12,16 +12,6 @@ if R_LIBS:
     import rpy2.robjects.numpy2ri as rn
     import rpy2.robjects as ro
     rn.activate()
-    install_SID = False 
-    if install_SID:
-        # import rpy2's package module
-        import rpy2.robjects.packages as rpackages
-        # import R's utility package
-        utils = rpackages.importr('utils')
-        # select a mirror for R packages
-        utils.chooseCRANmirror(ind=1) # select the first mirror in the list
-    
-
     ro.r("""library(SID)""")
 
 
